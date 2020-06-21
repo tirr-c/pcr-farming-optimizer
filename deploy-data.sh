eac9db580c16b1cb201eff8d58c4001d37426cbf
Wonwoo Choi <chwo9843@gmail.com>
2020-06-21 16:11:33 +0900

    deploy-data.sh: Use static/ instead of public/

--- deploy-data.sh
+++ deploy-data.sh
@@ -4,4 +4,4 @@ set -euo pipefail; IFS=$'\n\t'
 aws s3 --region us-east-1 sync \
   --acl public-read --cache-control 'no-cache' \
   --delete \
-  public/data/ "${BUCKET}/farming-optimizer/data/"
+  static/data/ "${BUCKET}/farming-optimizer/data/"
