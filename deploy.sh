19e79664c1c317e89cf28b79be9cf64b7934875d
Wonwoo Choi <chwo9843@gmail.com>
2020-07-29 22:41:45 +0900

    Do not delete previous builds during the deploy

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,10 @@ set -euo pipefail; IFS=$'\n\t'
 GATSBY_TELEMETRY_DISABLED=1 yarn build --prefix-paths
 aws s3 sync \
   --acl public-read \
-  --delete --exclude 'static/*' --exclude 'data/*' --exclude 'sw.js' \
+  --exclude 'static/*' --exclude 'data/*' --exclude 'sw.js' \
   public/ "${BUCKET}/farming-optimizer/"
 aws s3 sync \
   --acl public-read --cache-control 'public, max-age=604800, immutable' \
-  --delete \
   public/static/ "${BUCKET}/farming-optimizer/static/"
 aws s3 cp \
   --acl public-read --cache-control 'no-store' \
