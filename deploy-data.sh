0539d16c82dbca1d1aad8352196adae769c440e4
Wonwoo Choi <chwo9843@gmail.com>
2020-07-29 21:13:00 +0900

    Use AWS_REGION env

--- deploy-data.sh
+++ deploy-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail; IFS=$'\n\t'
 
-aws s3 --region us-east-1 sync \
+aws s3 sync \
   --acl public-read --cache-control 'no-cache' \
   --delete --size-only \
   static/data/ "${BUCKET}/farming-optimizer/data/"
